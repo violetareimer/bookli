72efa7b1edf639810dfb6913672d91625dc6a768
ivanduva <duva.ivan@outlook.com>
2020-04-05 13:56:37 -0300

    Agrega modelo de libro

---
+++ server/src/models/book.js
@@ -0,0 +1,48 @@
+const Sequelize = require('sequelize')
+
+const db = require('../db.js')
+
+const Book = db.define('Book', {
+	// attributes
+	title: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	synopsis: {
+		type: Sequelize.STRING
+	},
+	year: {
+		type: Sequelize.INTEGER,
+		allowNull: false
+	},
+	publisher: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	isbn: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	genres: {
+		type: Sequelize.JSON
+	},
+	authors: {
+		type: Sequelize.JSON,
+		allowNull: false
+	},
+	cover: {
+		type: Sequelize.BLOB
+	}
+}, { tableName: 'Book' })
+
+const getAllBooks = () => Book.findAll()
+
+const createBook = (data) => Book.create(data)
+
+const BookModel = {
+	Book: Book,
+	getAll: getAllBooks,
+	create: createBook,
+}
+
+module.exports = BookModel
---
+++ server/src/models/index.js
@@ -0,0 +1,16 @@
+const BookModel = require('./book.js');
+
+async function createTables() {
+    return BookModel.Book.sync();
+}
+
+async function dropTables() {
+    BookModel.Book.destroy({
+        where: {}
+    });
+}
+
+module.exports = {
+    createTables,
+    dropTables
+}
