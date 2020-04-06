9de8e1dcbb3e6ff9a646a29ad670b538140404b5
ivanduva <duva.ivan@outlook.com>
2020-04-06 03:10:36 -0300

    Atomiza tests de unidad

--- test/unit/model.test.js
+++ test/unit/model.test.js
@@ -102,13 +102,77 @@ test('Buscar libros', async () => {
 
     // Creamos los libros
     const firstBook = await BookModels.create(firstBookData)
-    const secondBook = await BookModels.create(secondBookData)
+    await BookModels.create(secondBookData)
 
     // Obtenemos los libros filtrados
     const filterBook = await BookModels.getAll('pragmatic');
-    const filterEmpty = await BookModels.getAll('something');
 
     expect(filterBook.length).toBe(1);
     expect(firstBook.id).toBe(filterBook[0].id);
+});
+
+test('Buscar libros sin resultado', async () => {
+    const firstBookData = {
+        title: 'The Pragmatic Programmer',
+        synopsis: 'Straight from the programming trenches, The Pragmatic Programmer cuts through the increasing specialization and technicalities of modern software development to examine the core process--taking a requirement and producing working, maintainable code that delights its users. It covers topics ranging from personal responsibility and career development to architectural techniques for keeping your code flexible and easy to adapt and reuse.',
+        year: 1999,
+        publisher: 'Addison-Wesley Professional',
+        isbn: '9780201616224',
+        genres: ['Educación', 'Tecnología', 'Programación'],
+        authors: ['David Thomas', 'Andrew Hunt'],
+        cover: '/assets/pragmatic-programmer.jpg'
+    };
+
+    const secondBookData = {
+        title: 'El Aleph',
+        synopsis: 'Este volumen reúne dieciocho relatos de Jorge Luis Borges, entre ellos quizá los más elogiados y repetidamente citados. Tanto «El inmortal» como «Los teólogos», «Deutsches Requiem» y «La espera» muestran las posibilidades expresivas de la «estética de la inteligencia» borgiana, inimitable fusión de mentalidad matemática, profundidad metafísica y captación poética del mundo.',
+        year: 1949,
+        publisher: 'Editorial Losada',
+        isbn: '9788499089515',
+        genres: ['Cuentos', 'Fantástico'],
+        authors: ['Jorge Luis Borges'],
+        cover: '/assets/el-aleph.jpg'
+    };
+
+    // Creamos los libros
+    await BookModels.create(firstBookData)
+    await BookModels.create(secondBookData)
+
+    // Obtenemos los libros filtrados
+    const filterEmpty = await BookModels.getAll('something');
+
     expect(filterEmpty.length).toBe(0);
+});
+
+test('Buscar libros con varios resultados', async () => {
+    const firstBookData = {
+        title: 'The Pragmatic Programmer',
+        synopsis: 'Straight from the programming trenches, The Pragmatic Programmer cuts through the increasing specialization and technicalities of modern software development to examine the core process--taking a requirement and producing working, maintainable code that delights its users. It covers topics ranging from personal responsibility and career development to architectural techniques for keeping your code flexible and easy to adapt and reuse.',
+        year: 1999,
+        publisher: 'Addison-Wesley Professional',
+        isbn: '9780201616224',
+        genres: ['Educación', 'Tecnología', 'Programación'],
+        authors: ['David Thomas', 'Andrew Hunt'],
+        cover: '/assets/pragmatic-programmer.jpg'
+    };
+
+    const secondBookData = {
+        title: 'El Aleph',
+        synopsis: 'Este volumen reúne dieciocho relatos de Jorge Luis Borges, entre ellos quizá los más elogiados y repetidamente citados. Tanto «El inmortal» como «Los teólogos», «Deutsches Requiem» y «La espera» muestran las posibilidades expresivas de la «estética de la inteligencia» borgiana, inimitable fusión de mentalidad matemática, profundidad metafísica y captación poética del mundo.',
+        year: 1949,
+        publisher: 'Editorial Losada',
+        isbn: '9788499089515',
+        genres: ['Cuentos', 'Fantástico'],
+        authors: ['Jorge Luis Borges'],
+        cover: '/assets/el-aleph.jpg'
+    };
+
+    // Creamos los libros
+    await BookModels.create(firstBookData)
+    await BookModels.create(secondBookData)
+
+    // Obtenemos los libros filtrados
+    const filterBooks = await BookModels.getAll('978');
+
+    expect(filterBooks.length).toBe(2);
 });
