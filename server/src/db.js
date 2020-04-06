f26d9651db97d24db715edf9bf18fc2248dadc1d
Rodrigo Jacznik <rodrigo.jacznik@501labs.com.ar>
2020-04-05 22:45:21 -0300

    Evita warning de opcion deprecada

--- server/src/db.js
+++ server/src/db.js
@@ -5,7 +5,7 @@ const inTest = process.env.NODE_ENV === 'test'
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
-    logging: !inTest,
+    logging: inTest ? false : console.log,
     storage: inTest
         ? ':memory:'
         : path.join(__dirname, '..', 'database.sqlite')
