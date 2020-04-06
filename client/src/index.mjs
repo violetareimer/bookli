c6c68b85589a682d738e84ed804f32a156bc6ddc
Rodrigo Jacznik <rodrigo.jacznik@501labs.com.ar>
2020-04-05 23:23:44 -0300

    Agrega mensaje para cuando no hay libros disponibles

--- client/src/index.mjs
+++ client/src/index.mjs
@@ -1,18 +1,23 @@
 import { initComponents } from './core/index.mjs';
 import BookServices from './services/book.mjs';
 
-const $bookList = document.querySelector('.booklist');
-
 async function initApp() {
+    const $bookList = document.querySelector('.booklist');
     const components = await initComponents();
 
     const BookComponent = components.book;
     const books = await BookServices.getAll();
 
-    books.forEach(function (book) {
-        const comp = new BookComponent(book);
-        $bookList.appendChild(comp);
-    });
+    // TODO: refactor a componente
+    if (books.length) {
+        books.forEach(function (book) {
+            const comp = new BookComponent(book);
+            $bookList.appendChild(comp);
+        });
+    } else {
+        $bookList.classList.add('booklist--empty');
+        $bookList.innerText = "Hmmm... Parece que nuestra libreria esta vacia.";
+    }
 }
 
 initApp();
