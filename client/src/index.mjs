a53d79404549c099dbc1ba4fd31a66165f37877d
Rodrigo Jacznik <rodrigo.jacznik@501labs.com.ar>
2020-04-05 22:44:26 -0300

    Agrega inicializacion de la app

--- client/src/index.mjs
+++ client/src/index.mjs
@@ -1,1 +1,18 @@
-console.log('Hola Bookli')
+import { initComponents } from './core/index.mjs';
+import BookServices from './services/book.mjs';
+
+const $bookList = document.querySelector('.booklist');
+
+async function initApp() {
+    const components = await initComponents();
+
+    const BookComponent = components.book;
+    const books = await BookServices.getAll();
+
+    books.forEach(function (book) {
+        const comp = new BookComponent(book);
+        $bookList.appendChild(comp);
+    });
+}
+
+initApp();
