07ae7f497d5199f54f6d4e62685841c2e6bbb067
Rodrigo Jacznik <rodrigo.jacznik@501labs.com.ar>
2020-04-19 19:12:06 -0300

    Remueve el viejo frontend

--- client/src/components/authors/authors.mjs
+++
@@ -1,8 +0,0 @@
-export default {
-    props: {
-        authors: Array
-    },
-    render: function (props) {
-        return `<p class="book__author">by ${ props.authors.join(', ') }</p>`
-    }
-}
